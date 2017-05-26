60575e26bddc48d5bcae4de3fb800c40cd632ec1
ito <ito_ma@ap-com.co.jp>
2017-05-26 17:56:34 +0900

    add distribution sample.

--- src/FactoryGas.php
+++ src/FactoryGas.php
@@ -2,68 +2,72 @@
 
 namespace imasami\FactoryGas;
 
+use Fuel\Core\DB;
+
 class FactoryGas
 {
-	private static $instance;
+  private static $instance;
 
-	protected $factories = [];
+  protected $factories = [];
 
-	private function __construct()
-	{
-	}
+  private function __construct()
+  {
+  }
 
-	public static function getInstance()
-	{
-		if (self::$instance == null) {
-			self::$instance = new FactoryGas();
-		}
-		return self::$instance;
-	}
+  public static function getInstance()
+  {
+    if (self::$instance == null) {
+      self::$instance = new FactoryGas();
+    }
+    return self::$instance;
+  }
 
-	/**
-	 * define factory.
-	 *
-	 * @param $name
-	 * @param array $kvs
-	 */
-	public function define($table, $name, array $kvs)
-	{
-		$this->factories[$name] = [
-			'table' => $table,
-			'param' => $kvs
-		];
-	}
+  /**
+   * define factory.
+   *
+   * @param $name
+   * @param array $kvs
+   */
+  public function define($table, $name, array $kvs)
+  {
+    $this->factories[$name] = [
+      'table' => $table,
+      'param' => $kvs
+    ];
+  }
 
-	/**
-	 * create array in memory.
-	 *
-	 * @param $name
-	 *
-	 * @return mixed
-	 */
-	public function build($name)
-	{
-		return $this->factories[$name]['param'];
-	}
+  /**
+   * create array in memory.
+   *
+   * @param $name
+   *
+   * @return mixed
+   */
+  public function build($name)
+  {
+    return $this->factories[$name]['param'];
+  }
 
-	/**
-	 * create db record.
-	 *
-	 * @param $name
-	 *
-	 * @return object
-	 */
-	public function create($name)
-	{
-		DB::insert($this->factories[$name]['table'])->set($this->factories[$name]['param'])->execute();
-		return DB::select()->from($this->factories[$name]['table'])->where($this->factories[$name]['param'])->execute();
-	}
+  /**
+   * create db record.
+   *
+   * @param $name
+   *
+   * @return object
+   */
+  public function create($name)
+  {
+    DB::insert($this->factories[$name]['table'])->set($this->factories[$name]['param'])->execute();
+    return DB::select()->from($this->factories[$name]['table'])->where($this->factories[$name]['param'])->execute();
+  }
 
-	/**
-	 * @return array
-	 */
-	public function getFactories()
-	{
-		return $this->factories;
-	}
+  /**
+   * @return array
+   */
+  public function getFactories()
+  {
+    return $this->factories;
+  }
 }
+
+require_once APPPATH.'tests/factories.php';
