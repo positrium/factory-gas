980aae3c181b58cf98dfab6e48749bf3776336d6
ito <ito_ma@ap-com.co.jp>
2017-06-06 17:53:48 +0900

    add include factories file test

---
+++ tests/factories/selftest_1_factory.php
@@ -0,0 +1,10 @@
+<?php
+
+use imasami\FactoryGas\FactoryGas;
+
+// ---------------------------------------------------------------------------
+
+FactoryGas::define('sample_users', 'sample_defined_on_factory_1', [
+    'COLUMN_1' => 1,
+    'COLUMN_2' => 9,
+]);
