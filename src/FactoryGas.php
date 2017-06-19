48504f06b4cf7903745679d7933f10d8523a557b
ITO Masami <ito_ma@ap-com.co.jp>
2017-06-19 13:34:13 +0900

    Update FactoryGas.php

--- src/FactoryGas.php
+++ src/FactoryGas.php
@@ -76,7 +76,6 @@ class FactoryGas
     {
         self::getInstance();
 
-			  DB::set_charset('utf8');
         DB::insert(self::$factories[$name]['table'])->set(self::$factories[$name]['param'])->execute();
         return DB::select()->from(self::$factories[$name]['table'])->where(self::$factories[$name]['param'])->execute()->current();
     }
